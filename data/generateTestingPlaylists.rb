0fef1b89a2008c601e6aab71cfb6b3814280861f
Nikola <nikola.stefancic@gmail.com>
2025-01-28 15:08:44 +0100

    Add new playlist entry and replace old random playlist generator with a new implementation

--- data/generateTestingPlaylists.rb
+++ data/generateTestingPlaylists.rb
@@ -1,20 +1,20 @@
 require 'json'
 
-def generate_random_playlists(num_playlists, num_songs_per_playlist)
+def generateRandomPlaylistsJSON(numPlaylists, numSongsPerPlaylist)
   playlists = []
 
-  num_playlists.times do |i|
+  numPlaylists.times do |i|
     playlist_name = "Playlist#{i + 1}"
     songs = []
 
-    num_songs_per_playlist.times do
-      song_name = "Song#{rand(100..999)}"
+    numSongsPerPlaylist.times do
+      songName = "Song#{rand(100..999)}"
       artist = "Artist#{rand(1..50)}"
       album = ["Album#{rand(1..20)}", "/"].sample
       duration = rand(1.0..10.0).round(2)
 
       song = {
-        "naziv" => song_name,
+        "naziv" => songName,
         "izvodac" => artist,
         "album" => album,
         "trajanje" => duration
@@ -32,10 +32,8 @@ def generate_random_playlists(num_playlists, num_songs_per_playlist)
   playlists
 end
 
-# Generate 5 playlists with 10 songs each
-random_playlists = generate_random_playlists(5, 10)
+randomPlaylists = generateRandomPlaylistsJSON(5, 10)
 
-# Write to a JSON file
 File.open("playlists.json", "w") do |file|
   file.write(JSON.pretty_generate(random_playlists))
 end
